8fa46763b7f9a0c2f958dec698b4420dc046fec6
weiloon617 <weiloon617@gmail.com>
2019-09-03 11:03:02 +0800

    done vue-advance-comp

---
+++ vue-advance-comp/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuotes'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr/>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+
+                <!--<app-quotes>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>A wonderful Quote</p>
+                </app-quotes>-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
+
+    export default {
+        data: function () {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuotes',
+            }
+        },
+        components: {
+            appQuotes: Quote,
+            appAuthor: Author,
+            appNew: New,
+        },
+    }
+</script>
+
+<style>
+</style>
---
+++ vue-advance-comp/src/components/New.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+        <button @click="counter++">Increase!</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'New',
+  data: function() {
+    return {
+      counter: 0,
+    }
+  },
+  destroyed() {
+    console.log('destroy!')
+  },
+  deactivated() {
+    console.log('deactivated!')
+  },
+  activated() {
+    console.log('activated!')
+  },
+}
+</script>
+
+<style scoped>
+</style>
