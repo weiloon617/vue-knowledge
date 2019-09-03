44f34a706d45d770ba0d2ff4ab1f60363b64b9a4
weiloon617 <weiloon617@gmail.com>
2019-09-03 11:49:37 +0800

    done wonderful-quote

---
+++ wonderful-quote/src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" v-bind:key="index" @click.native="deleteQuote(index)">{{ quote }}
+        </app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+
+export default {
+  name: 'QuoteGrid',
+  props: ['quotes'],
+  components: {
+    appQuote: Quote,
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit('quoteDeleted', index)
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
