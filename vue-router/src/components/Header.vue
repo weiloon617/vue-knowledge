959c9c5b7288237cb1aeba274e6de743686c3b9e
weiloon <weiloon617@gmail.com>
2019-09-15 22:05:06 +0800

    half way done vue-router

---
+++ vue-router/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="nav nav-pills">
+    <router-link to="/" tag="li" active-class="active" exact
+      ><a>Home</a></router-link
+    >
+    <router-link to="/user" tag="li" active-class="active"
+      ><a>User</a></router-link
+    >
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped></style>
