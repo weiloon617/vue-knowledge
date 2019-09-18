e2dada74ce0976cb031c6c5fa01f7e372c6c9698
G117E19030002 <g117e19030002@G117E19030002s-MacBook-Air.local>
2019-09-18 10:01:23 +0800

    done vue-server

--- vue-server/src/App.vue
+++ vue-server/src/App.vue
@@ -13,7 +13,12 @@
         </div>
         <button class="btn btn-primary" @click="submit">Submit</button>
         <hr />
+        <input type="text" class="form-control" v-model="node" />
+        <br />
+        <br />
         <button class="btn btn-primary" @click="fetchData">Get Data</button>
+        <br />
+        <br />
         <ul class="list-group">
           <li
             class="list-group-item"
@@ -36,25 +41,42 @@ export default {
         username: "",
         email: ""
       },
-      users: []
+      users: [],
+      resource: {},
+      node: ""
     };
   },
   methods: {
     submit() {
-      this.$http
-        .post("https://vue-http-ae4be.firebaseio.com/data.json", this.user)
-        .then(
-          response => {
-            console.log(response);
-          },
-          error => {
-            console.log(error);
-          }
-        );
+      /*this.$http.post("data.json", this.user).then(
+        response => {
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        }
+      );*/
+
+      // this.resource.save({}, this.user);
+
+      this.resource.saveAlt(this.user);
     },
     fetchData() {
-      this.$http
-        .get("https://vue-http-ae4be.firebaseio.com/data.json")
+      /*this.$http
+        .get("data.json")
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });*/
+
+      this.resource
+        .getData({ node: this.node })
         .then(response => {
           return response.json();
         })
@@ -66,6 +88,13 @@ export default {
           this.users = resultArray;
         });
     }
+  },
+  created() {
+    const customActions = {
+      saveAlt: { method: "POST", url: "alternative.json" },
+      getData: { method: "GET" }
+    };
+    this.resource = this.$resource("{node}.json", {}, customActions);
   }
 };
 </script>
--- vue-server/src/main.js
+++ vue-server/src/main.js
@@ -4,6 +4,22 @@ import VueResource from "vue-resource";
 
 Vue.use(VueResource);
 
+Vue.http.options.root = "https://vue-http-ae4be.firebaseio.com/";
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method === "POST") {
+    request.method = "PUT";
+  }
+
+  next(response => {
+    response.json = () => {
+      return {
+        messages: response.body
+      };
+    };
+  });
+});
+
 new Vue({
   el: "#app",
   render: h => h(App)
