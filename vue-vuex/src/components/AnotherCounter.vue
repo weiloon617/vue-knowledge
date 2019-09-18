2b17c3a6e5bb7ed697bead8e4eeaafd31a3c87af
G117E19030002 <g117e19030002@G117E19030002s-MacBook-Air.local>
2019-09-17 17:12:36 -0700

    done vue-vuex

---
+++ vue-vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button
+      class="btn btn-primary"
+      @click="asyncIncrement({ by: 50, duration: 500 })"
+    >
+      Increment
+    </button>
+    <button
+      class="btn btn-primary"
+      @click="asyncDecrement({ by: 50, duration: 500 })"
+    >
+      Decrement
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "AnotherCounter",
+  methods: {
+    ...mapActions(["asyncIncrement", "asyncDecrement"])
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ vue-vuex/src/components/AnotherResult.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "AnotherResult",
+  /*computed: {
+    counter() {
+      return this.$store.getters.doubleCounter;
+    },
+    clicks() {
+      return this.$store.getters.stringCounter;
+    }
+  }*/
+  computed: {
+    ...mapGetters(["doubleCounter", "stringCounter"])
+  }
+};
+</script>
+
+<style scoped></style>
