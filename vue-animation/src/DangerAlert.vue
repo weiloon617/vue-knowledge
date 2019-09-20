f3fe068de255aaae0c80256cd21348d86280a27c
G117E19030002 <g117e19030002@G117E19030002s-MacBook-Air.local>
2019-09-20 15:52:58 +0800

    done vue-animation

---
+++ vue-animation/src/DangerAlert.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="alert alert-danger">This is dangerous!</div>
+</template>
+
+<script>
+export default {
+  name: "DangerAlert"
+};
+</script>
+
+<style scoped></style>
---
+++ vue-animation/src/SuccessAlert.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="alert alert-success">This is successful!</div>
+</template>
+
+<script>
+export default {
+  name: "SuccessAlert"
+};
+</script>
+
+<style scoped></style>
