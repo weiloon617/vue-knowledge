32001b399ee2569e590e1baa81b66e1bc48edf2d
G117E19030002 <g117e19030002@G117E19030002s-MacBook-Air.local>
2019-09-26 10:15:26 +0800

    done nuxt-router-project

--- nuxt-router-project/nuxt.config.js
+++ nuxt-router-project/nuxt.config.js
@@ -1,3 +1,5 @@
+const bodyParser = require("body-parser");
+
 export default {
   mode: "universal",
   /*
@@ -62,5 +64,6 @@ export default {
   transition: {
     name: "fade",
     mode: "out-in"
-  }
+  },
+  serverMiddleware: [bodyParser.json(), "~/api"]
 };
--- nuxt-router-project/store/index.js
+++ nuxt-router-project/store/index.js
@@ -96,6 +96,10 @@ const createStore = () => {
               "expirationDate",
               new Date().getTime() + Number.parseInt(result.expiresIn) * 1000
             );
+
+            return this.$axios.$post("http://localhost:3000/api/track-data", {
+              data: "Authenticated"
+            });
           })
           .catch(error => console.log(error));
       },
